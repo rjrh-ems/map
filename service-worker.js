518a9fbd6d1989e7211eb20c231133a9cdb81c74
rjrh-ems <ems.ruamjairakhospital@gmail.com>
2026-05-04 16:47:48 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'ems-map-pwa-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).catch(() => null));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  const url = new URL(req.url);
+  if (req.method !== 'GET' || url.hostname.includes('supabase.co') || url.hostname.includes('nominatim.openstreetmap.org')) return;
+  event.respondWith(caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+    const copy = res.clone();
+    caches.open(CACHE_NAME).then((cache) => cache.put(req, copy)).catch(() => null);
+    return res;
+  }).catch(() => caches.match('./index.html'))));
+});
